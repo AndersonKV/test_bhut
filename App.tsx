443b0f0e328cb6b1ae13f357590a6170ac8e7387
unknown <noitegarden@gmail.com>
2022-09-14 17:12:12 -0300

    startando projeto

---
+++ App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>iniciando</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
