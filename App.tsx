75ff6e8dbeec57dce32cd40e8c8bbde26e33229a
unknown <noitegarden@gmail.com>
2022-09-15 19:08:14 -0300

    agora sim o ultimo

--- App.tsx
+++ App.tsx
@@ -1,12 +1,4 @@
-/* eslint-disable react/react-in-jsx-scope */
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Pressable,
-} from 'react-native';
 import Routes from './src/routes';
 
 export default function App() {
